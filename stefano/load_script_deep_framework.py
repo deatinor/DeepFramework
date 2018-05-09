aa9674ffba256c7bc72d78a566fe3ddb4a298d42
Leonardo Petrini <leonardo.petrini@epfl.ch>
2018-05-09 14:32:01 +0200

    Added bias modified dataset

--- stefano/load_script_deep_framework.py
+++ stefano/load_script_deep_framework.py
@@ -4,12 +4,13 @@ import math
 # Import default load script from parent folder
 
 def load_dataset():
-    train=torch.rand([1000,2])
-    target=torch.ones([1000,2])
-    target_bool=train.norm(p=2,dim=1)
+    train = torch.rand([1000,2])
+    target = torch.ones([1000,2])
+
+    target_bool=(train - torch.Tensor([0.5, 0.5]).view(1,-1)).norm(p=2,dim=1)
     target_bool=target_bool<1/math.sqrt(2*math.pi)
-    target[:,0][target_bool==1]=-1
-    target[:,1][target_bool==0]=-1
 
-    return train,target
+    target[:,0][target_bool == 1] = -1
+    target[:,1][target_bool == 0] = -1
 
+    return train, target
